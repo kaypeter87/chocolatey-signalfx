e492fa6b7b80bec5dcef543c77b4c94863f10c1c
Peter Kay <contact@kaypeter.com>
2019-09-25 12:09:57 -0400

    add comments to operations

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -5,11 +5,13 @@ $configDir = "$($env:ProgramData)\SignalFxAgent"
 
 $service = Get-Service "signalfx-agent" -ErrorAction SilentlyContinue
 
+# Use agent binary arguments to uninstall
 if ($null -ne $service)
 {
   $uninstallArgs = '-service "uninstall"'
   Start-Process -FilePath "$destinationDir\SignalFxAgent\bin\signalfx-agent.exe" -ArgumentList $uninstallArgs -NoNewWindow
 }
 
+# Clean up directories
 Remove-Item -Recurse -Force -Path $destinationDir
 Remove-Item -Recurse -Force -Path $configDir
